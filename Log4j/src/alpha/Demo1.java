ce9a44965e1db3be16f22e05c7118b2db494fe47
Sam Smith <sam@example.com>
2022-07-13 16:37:18 +0530

    log4j first commit

---
+++ Log4j/src/alpha/Demo1.java
@@ -0,0 +1,17 @@
+package alpha;
+
+import org.apache.logging.log4j.*;
+public class Demo1 {
+
+	private static Logger log= LogManager.getLogger(Demo1.class.getName());
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		log.debug("button is clicked");
+		log.info("button click success");
+		log.error("button not clicked");
+		log.fatal("button not visible");
+		
+	}
+  
+}
